6ee54952605c0f7b19f4c056f4df6b1ed3262e5e
fujinuma <goldenchain0804@gmail.com>
2026-04-25 02:55:34 +0900

    third

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './mahjong_danran_mvp_prototype.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
